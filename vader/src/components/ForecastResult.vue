e18b086a2d79b4d15d50e2fb78cced755b1b249b
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2024-10-16 09:14:00 +0300

    Prognossidan klar

--- vader/src/components/ForecastResult.vue
+++ vader/src/components/ForecastResult.vue
@@ -1,25 +1,46 @@
 <script setup>
+import wc from "@/assets/data/weatherCodes.json"
+import { ref } from "vue";
 const props = defineProps(['forecast'])
+const weatherCodes = ref(wc)
+
+function getText(code) {
+    let wcText = weatherCodes.value.find(itm => {
+        return itm.code == code
+    }).description ?? 'Unknown'
+
+    return wcText
+}
 </script>
 <template>
     <ul>
         <li>Date</li>
-        <li>Code</li>
         <li>Temp</li>
         <li>Precip</li>
         <li>Wind</li>
     </ul>
     <ul v-for="day in props.forecast.weather" :key="day">
-        <li>{{ day.date }}</li>
-        <li>{{ day.code }}</li>
-        <li>{{ day.temp }}</li>
-        <li>{{ day.precipitation }}</li>
-        <li>{{ day.wind }}</li>
+        <li>
+            {{ new Date(day.date).getDate() }}.{{ new Date(day.date).getMonth() + 1 }}
+            {{ getText(day.code) }}
+        </li>
+        <li>{{ day.temp.min }}{{ day.temp.unit }} - {{ day.temp.max }}{{ day.temp.unit }}</li>
+        <li>
+            {{ day.precipitation.sum }}{{ day.precipitation.unit }}<br>
+            ({{ day.precipitation.probability }}%)
+        </li>
+        <li>
+            {{ Math.round(day.wind.speed) }} ({{ Math.round(day.wind.gusts) }}){{ day.wind.unit }}<br>{{
+                day.wind.direction }}{{
+                day.wind.direction_unit }}
+        </li>
     </ul>
 </template>
 <style scoped>
 ul {
     padding: 0;
+    display: grid;
+    grid-template-columns: 22% 22% 15% auto;
 }
 
 ul:nth-child(even) {
@@ -33,6 +54,6 @@ ul:nth-child(odd) {
 li {
     padding: 0 .5em;
     list-style-type: none;
-    display: inline-block;
+    vertical-align: top;
 }
 </style>
